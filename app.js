0c3951513f74b37ce68410113fefd194c59c8f4c
heysoysimon <77466098+heysoysimon@users.noreply.github.com>
2022-04-30 12:21:12 -0400

    primer commit

---
+++ app.js
@@ -0,0 +1,106 @@
+// slider 
+let slider = document.querySelector(".slider-contenedor")
+let sliderIndividual = document.querySelectorAll(".contenido-slider")
+let contador = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 5000;
+
+let value;
+
+window.addEventListener("resize", function(){
+    width = sliderIndividual[0].clientWidth;
+})
+
+setInterval(function(){
+    slides();
+},intervalo);
+
+
+function changerPosition(change){
+    console.log(change)
+}
+function slides(){
+    slider.style.transform = "translate("+(-width*contador)+"px)";
+    slider.style.transition = "transform .8s";
+    contador++;
+
+    if(contador == sliderIndividual.length){
+        setTimeout(function(){
+            slider.style.transform = "translate(0px)";
+            slider.style.transition = "transform 0s";
+            contador=1;
+        },2500)
+    }
+}
+
+// menu responsive
+
+let boton = document.getElementById('icono')
+let enlaces = document.getElementById('enlaces')
+let contadorMenu = 0
+let DOM = document.getElementById('DOM')
+
+boton.addEventListener("click", e  => {
+    if(contadorMenu ==0){
+        enlaces.className = ("enlaces dos")
+        contadorMenu=1
+    }else{
+        enlaces.classList.remove('dos')
+        enlaces.className = ('enlaces uno')
+        enlaces.className=('enlaces uno')
+        contadorMenu = 0
+        
+    }
+})
+
+// animaciones scroll
+
+const ubicacion = window.pageYOffset
+
+window.addEventListener('scroll', function(){
+    console.log(window.pageYOffset)
+    AOS.init();
+   
+})
+
+// window.addEventListener('click', function(e){
+//     console.log(e.target);
+//     if(e.target == DOM){
+//         enlaces.style.display = 'none';
+//     }
+// })
+
+// ventana modal 
+// window.onload = function(){
+// let modal = document.getElementById('miModal');
+// let abrir2 = document.getElementById('abrir2');
+// let abrir = document.getElementById('abrir');
+// let cerrar = document.getElementById('close');
+// let flex = document.getElementById('flex');
+
+// modalWindow(modal, abrir, abrir2,cerrar,flex)
+// }
+
+
+
+// function modalWindow(modal, abrir, abrir2,cerrar,flex){
+
+//      cerrar.addEventListener('click', e =>{
+//          modal.style.display = 'none'
+//      })
+
+
+//      abrir2.addEventListener('click', e =>{
+//         modal.style.display = 'block';
+//     })
+//      abrir.addEventListener('click', e =>{
+//         modal.style.display = 'block';
+//     })
+
+//     window.addEventListener('click', function(e){
+//         console.log(e.target);
+//         if(e.target == flex){
+//             modal.style.display = 'none';
+//         }
+//     });
+// }
